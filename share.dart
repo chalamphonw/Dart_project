cc2d5564b19c3854c82a957acddd541e4e2d5a17
chalamphonw <ifone410@gmail.com>
2024-11-19 23:49:40 +0700

    65311132

---
+++ share.dart
@@ -0,0 +1,21 @@
+import 'dart:io';
+void main () {
+
+  // รับยอดบิลทั้งหมดจากผู้ใช้
+  print('กรุณากรอกยอดบิลทั้งหมด:');
+  double Bill = double.parse(stdin.readLineSync()!);
+
+  // รับจำนวนคนที่ร่วมทานอาหาร
+  print('กรุณากรอกจำนวนคนที่ร่วมทาน:');
+  int numberOfPeople = int.parse(stdin.readLineSync()!);
+
+  // คำนวณยอดที่แต่ละคนต้องจ่าย
+  double amountPerPerson = Bill / numberOfPeople;
+
+  // แสดงผล
+  print('ยอดที่แต่ละคนต้องจ่ายคือ: ฿$amountPerPerson');
+
+}
+git add .
+git commit -m "65311132"
+git push
