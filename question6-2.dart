16e9ed57a58fcacad76925b26311c79056a1acb6
Your Name <your-email@example.com>
2024-12-02 15:36:49 +0700

    65311132

--- question6-2.dart
+++ question6-2.dart
@@ -6,14 +6,14 @@ class House {
   House(this.id, this.name, this.price);
 
   void display() {
-    print("ID: $id, Name: $name, Price: \$${price.toStringAsFixed(2)}");
+    print("ID: $id, Name: $name, Price: \$${price.toStringAsFixed(0)}");
   }
 }
 
 void main() {
-  House house1 = House(1, "Cozy Cottage", 120000.50);
-  House house2 = House(2, "Modern Villa", 300000.75);
-  House house3 = House(3, "Luxury Penthouse", 750000.00);
+  House house1 = House(1, "Cozy Cottage", 120000);
+  House house2 = House(2, "Modern Villa", 300000);
+  House house3 = House(3, "Luxury Penthouse", 750000);
 
   List<House> houseList = [house1, house2, house3];
 
